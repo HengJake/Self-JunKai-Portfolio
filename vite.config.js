88b2c8d6bde2301496d4661f5c1129d7ec301927
Heng Jun Kai <hengjunkai@gmail.com>
2026-04-19 09:05:36 +1000

    change domain name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/JunKai-Portfolio",
+  base: "/Self-JunKai-Portfolio",
   plugins: [react()],
 })
