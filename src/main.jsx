334add96beb7bed5b44d0429102d7405b20ff1a7
Heng Jake <hengjunkai@gmail.com>
2026-05-06 09:43:58 +1000

    update-name

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router basename="/JunKai-Portfolio">
+    <Router basename="/Self-JunKai-Portfolio">
       <App />
     </Router>
   </React.StrictMode>
